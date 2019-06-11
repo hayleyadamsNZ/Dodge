c55aee9d65ec9dabf83c04a21d7158ab9ac295b9
Hayley Adams <hayleyadams@mmc.school.nz>
2019-06-11 13:12:21 +1200

    Starting

---
+++ Dodge/Form1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dodge
+{
+    public partial class FrmDodge : Form
+    {
+        /// <summary>
+        /// Starting
+        /// </summary>
+        public FrmDodge()
+        {
+            InitializeComponent();
+        }
+    }
+}
